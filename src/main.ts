3e8e2bd1f10627863b0158be95aff7ca22784df4
YUUKIToriyama <github@toriyama.dev>
2021-07-05 21:20:01 +0900

    cacを導入した

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,30 @@
-import readline from 'readline';
-import chalk from 'chalk';
-import * as geolonia from '@geolonia/normalize-japanese-addresses';
+#!/usr/bin/env node
+import { cac } from 'cac';
+import { startRepl, callGeolonia } from './lib';
 
-const stdin: NodeJS.ReadableStream = process.stdin;
-const stdout: NodeJS.WritableStream = process.stdout;
+import fs from "fs";
+const { version } = JSON.parse(fs.readFileSync(__dirname + "/../package.json").toString("utf8"));
 
-const startRepl = () => {
-	const rl = readline.createInterface({
-		input: stdin,
-		output: stdout,
-		prompt: ">>> "
+const cli = cac("geolonia-nja-cli");
+cli.version(version, "-v, --version");
+cli.help();
+cli.option("-a, --address <string>", "住所を指定");
+cli.option("-l, --level <number>", "「深さ」を指定", {
+	default: 3
+});
+
+const parsed = cli.parse();
+const level: number = parsed.options.level;
+const address = parsed.options.address;
+
+if (typeof address === "string") {
+	// 引数が指定されている場合
+	// その住所について正規化を行なう
+	callGeolonia(address, {
+		level: level
 	});
-
-	rl.prompt();
-	rl.on("line", (input) => {
-		geolonia.normalize(input).then(result => {
-			console.log(chalk.green(JSON.stringify(result, null, "\t")));
-		}).catch(error => {
-			console.error(chalk.red(error));
-		}).then(() => {
-			rl.setPrompt(">>> ");
-			rl.prompt();
-		});
-	}).on("close", () => {
-		console.log("Thanks to Geolonia Inc. for @geolonia/normalize-japanese-addresses");
-		process.exit(0);
-	});
+} else {
+	// 引数がない場合
+	// REPLを起動する
+	startRepl();
 }
-
-startRepl();
