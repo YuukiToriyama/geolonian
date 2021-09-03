86f9f7bd4c4d31d4ad914a991c41bb52e40916f2
YUUKIToriyama <github@toriyama.dev>
2021-09-03 21:00:03 +0900

    use caporal instead of cac

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,33 @@
 #!/usr/bin/env node
-import { cac } from 'cac';
+import * as Caporal from '@caporal/core';
 import { startRepl, callGeolonia } from './lib';
 
 import fs from "fs";
 const { version } = JSON.parse(fs.readFileSync(__dirname + "/../package.json").toString("utf8"));
 
-const cli = cac("geolonia-nja-cli");
-cli.version(version, "-v, --version");
-cli.help();
-cli.option("-a, --address <string>", "住所を指定");
-cli.option("-l, --level <number>", "「深さ」を指定", {
-	default: 3
-});
+Caporal.program
+	.name("geolonia-nja-cli")
+	.version(version)
+	.description("@geolonia/normalize-japanese-addressesをコンソール上で簡単にテストするためのモジュール");
 
-const parsed = cli.parse();
-const level: number = parsed.options.level;
-const address = parsed.options.address;
-
-if (typeof address === "string") {
-	// 引数が指定されている場合
-	// その住所について正規化を行なう
-	callGeolonia(address, {
-		level: level
+Caporal.program
+	.command("normalize", "Normalize an address")
+	.option("-a, --address <address>", "Address")
+	.option("-l, --level <depth>", "Depth of normalization", {
+		default: 3
+	})
+	.action(({ options }) => {
+		if (options.address != undefined) {
+			// 引数が指定されている場合
+			// その住所について正規化を行なう
+			callGeolonia(options.address as string, {
+				level: options.level as number
+			})
+		} else {
+			// 引数がない場合
+			// REPLを起動する
+			startRepl();
+		}
 	});
-} else {
-	// 引数がない場合
-	// REPLを起動する
-	startRepl();
-}
+
+Caporal.program.run();
